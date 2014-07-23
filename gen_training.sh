08904dc5f8a865b75bdb606940fd399914f3f35b
Sam Kelly <sam@durosoft.com>
2014-07-23 10:13:37 -0400

    parallelized training instance generator

--- gen_training.sh
+++ gen_training.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 ./build.sh
 cd bin
-raptor_3dsfile="/home/sam/Desktop/raptor/old_src/ships/hamina-class missile boat/Hamina.3DS" free_rot_vars=xyz plan=B start=1 end=2 blender -b -P render.py
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+start=1
+end=10000
+per=$((($end - $start) / $CORES))
+echo $per
+for (( i=0; i<CORES; i++ ))
+do
+	istart=$(($per * $i))
+	iend=$(($istart + $per - 1))
+	raptor_3dsfile="/home/sam/Desktop/raptor/old_src/ships/hamina-class missile boat/Hamina.3DS" free_rot_vars=xyz plan=B start=$istart end=$iend blender -b -P render.py &
+done
+istart=$(($per * $i))
+iend=$end
+raptor_3dsfile="/home/sam/Desktop/raptor/old_src/ships/hamina-class missile boat/Hamina.3DS" free_rot_vars=xyz plan=B start=$istart end=$iend blender -b -P render.py &
+
